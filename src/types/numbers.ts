e7eb632c9b65023ae6a24afefa0cd5b060ac5d63
Adam Cardenas <cevaris@gmail.com>
2020-10-08 12:39:09 +0000

    started numbers

---
+++ src/types/numbers.ts
@@ -0,0 +1,16 @@
+const one: number = 1;                    // integer or whole number 
+const pie: number = 3.141592653589793238; // decimal number
+
+var scientificFormatNumber: number = 13e3;         // 13000
+var anotherScientificFormatNumber: number = 11e-3; // 0.011
+
+var aSum: number = 3 + 44.43;
+console.log(aSum); // 47.43
+
+var multiplication: number = 1.25 * 5;
+console.log(multiplication); // 6.25
+
+
+console.log(8 / 4);    // 2, returns whole number if division returns no remainder
+console.log(3 / 4);    // 0.75, by default division returns decimal
+console.log(123 / 0);  // Infinity, you cannot divide a number by 0
