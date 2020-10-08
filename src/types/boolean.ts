faa78e07592fc4533ba6a9d7d612d25a2c0a3687
Adam Cardenas <cevaris@gmail.com>
2020-10-08 00:45:05 +0000

    adding more TODOs, wrapped up strings

---
+++ src/types/boolean.ts
@@ -0,0 +1,7 @@
+/**
+ * Boolean values can be either true or false.
+ * Boolean values make up the basis for making decisions in code. 
+ * e.x If some Boolean value is true, execute this code, otherwise skip.
+ */
+var boolValue = true;
+var boolValue = false;
